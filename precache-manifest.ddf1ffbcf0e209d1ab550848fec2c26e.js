c5dfa6bd3c20f279790ead7934a0749f19718814
rokingana <roking.ana@gmail.com>
2020-05-18 12:20:26 +0100

    Updates

---
+++ precache-manifest.ddf1ffbcf0e209d1ab550848fec2c26e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c4c1e1ed73072c2019326398a297b2",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "1b78e332736724ae6b32",
+    "url": "/todo-app/static/css/main.f37cda62.chunk.css"
+  },
+  {
+    "revision": "a8c5b671005c9abdf1e0",
+    "url": "/todo-app/static/js/2.0f102d56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.0f102d56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b78e332736724ae6b32",
+    "url": "/todo-app/static/js/main.a68a4967.chunk.js"
+  },
+  {
+    "revision": "2ea9dd0813ccd799bdfb",
+    "url": "/todo-app/static/js/runtime-main.83a162ee.js"
+  }
+]);
